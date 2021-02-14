5690f78ff88d742c3c1af6d9393730556c9f159f
”Junior” <rip05-04-1990@yandex.ru>
2021-02-14 22:47:44 +0300

    adding class Waiter

--- src/kitchen/Cook.java
+++ src/kitchen/Cook.java
@@ -5,8 +5,9 @@ import main.ConsoleHelper;
 import java.util.Observable;
 import java.util.Observer;
 
-public class Cook implements Observer {
+public class Cook extends Observable implements Observer {
     private String name;
+    private Observer observer;
 
     public Cook(String name) {
         this.name = name;
@@ -20,5 +21,7 @@ public class Cook implements Observer {
     @Override
     public void update(Observable o, Object arg) {
         ConsoleHelper.writeMessage("Start cooking - " + arg);
+        setChanged();
+        notifyObservers(arg);
     }
 }
--- src/main/Restaurant.java
+++ src/main/Restaurant.java
@@ -1,6 +1,7 @@
 package main;
 
 import kitchen.Cook;
+import kitchen.Waiter;
 
 import java.util.Observer;
 
@@ -8,8 +9,10 @@ public class Restaurant {
 
     public static void main(String[] args) {
         Tablet tablet1 = new Tablet(1);
-        Observer observer = new Cook("Amigo");
-        tablet1.addObserver(observer);
+        Cook cook = new Cook("Amigo");
+        Waiter waiter = new Waiter();
+        cook.addObserver(waiter);
+        tablet1.addObserver(cook);
         tablet1.createOrder();
         tablet1.createOrder();
 
