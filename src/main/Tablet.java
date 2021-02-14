ac8bd2988d138dd80986eeed2a8f981381420eb2
”Junior” <rip05-04-1990@yandex.ru>
2021-02-14 14:51:16 +0300

    adiing main classes: restaurant, order, tablet, dish

---
+++ src/main/Tablet.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task27.task2712;
+
+public class Tablet {
+    private final int number;
+
+    public Tablet(int number) {
+        this.number = number;
+    }
+
+    //создает заказ из тех блюд, которые выберет пользователь
+    public void createOrder(){
+
+    }
+
+}
