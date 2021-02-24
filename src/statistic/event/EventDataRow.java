8136283f020f154767977be435a12b66567f96bd
”Junior” <rip05-04-1990@yandex.ru>
2021-02-24 21:44:35 +0300

    adding package statistic

---
+++ src/statistic/event/EventDataRow.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task27.task2712.statistic.event;
+
+//интерфейс-маркер по нему мы определяем, является ли переданный объект событием или нет
+public interface EventDataRow {
+}
