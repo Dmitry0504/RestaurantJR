abe692cce31fdaa6cfedbda7cbd91da99b5a50ea
”Junior” <rip05-04-1990@yandex.ru>
2021-02-25 16:30:59 +0300

    changing StatisticManager

--- src/statistic/StatisticManager.java
+++ src/statistic/StatisticManager.java
@@ -2,10 +2,17 @@ package statistic;
 
 
 import statistic.event.EventDataRow;
+import statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 //будет регистрировать события в хранилище
 public class StatisticManager {
     private static StatisticManager instance;
+    private StatisticStorage statisticStorage = new StatisticStorage();
 
     private StatisticManager(){
     }
@@ -17,6 +24,17 @@ public class StatisticManager {
 
     public void register(EventDataRow data){
 
+    }
+
+    private class StatisticStorage{
+        private Map<EventType, List<EventDataRow>> storage;
+
+        public StatisticStorage() {
+            storage = new HashMap<>();
+            for(EventType event: EventType.values()){
+                storage.put(event, new ArrayList<>());
+            }
+        }
     }
 
 }
